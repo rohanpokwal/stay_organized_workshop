9be3e08858114ed7f6536c3e8f50731b93546505
Italy Gonzalez <italygonzalez2017@yahoo.com>
2024-06-14 15:26:54 -0500

    add users to the dropdown

--- scripts/todos.js
+++ scripts/todos.js
@@ -12,28 +12,58 @@ window.onload=()=>{
     //when a option is picked from the userdropdown a function to display data will appear
     userDropdown.addEventListener("change", displayTask);
 
+    //calling the function that puts information in dropdown
+    displayUser();
+
+}
+
+let displayTask = async ()=>{
+
+console.log("hi");
 
 
 }
-let displayTask = async ()=>{
+let displayUser = async ()=>{
 
     //grabbing dropdown from html
     let userDropdown = document.querySelector("#userSelect");
-    
+
     //getting a hold of the data fetched to display
     let allToDos = await getAllData();
 
+     //creating a default option to the dropwdown
+     let defaultOption = document.createElement("option");
+     defaultOption.value = "";
+     
+     defaultOption.innerText = "----Select User----";
+  
+     userDropdown.appendChild(defaultOption);
 
-    
+     //here a loop is run to apply the names we need inside the dropdown 
+    allToDos.forEach((user) => {
+
+        //create the new option for the category we are on in the loop
+        let newOption = document.createElement("option");
+
+        //set the value for the option
+        newOption.value = user.id;
+
+        //set what the user sees 
+        newOption.textContent = user.name;
+
+        userDropdown.appendChild(newOption);
+
+
+    })
 
 
 
 }
 //grabing the data needed to add to the dropdown as well as div
-async function getAllData() {
+let getAllData= async () =>{
 
     try {
-        let response = await fetch("http://localhost:8083/api/todos");
+        let response = await fetch("http://localhost:8083/api/users");
         let list = await response.json();
 
         return list;
